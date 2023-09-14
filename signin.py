1587d1b99f79b8eba5b4d5cee21f58fcc5bed2c5
Manoj Ghosh <manojghoshdk@gmail.com>
2023-09-14 12:52:14 +0530

    add text box for user name

--- signin.py
+++ signin.py
@@ -1,8 +1,24 @@
 from tkinter import *
 from PIL import ImageTk
 
+
+# Functionality Part
+
+
+
+
+
+
+
+
+# GUI Part
+
 root = Tk()
 root.geometry('990x660+50+50')
+root.resizable(0,0) # disabling the window size
+root.title('Login Page') #adding title of the page
+
+
 
 bgImage = ImageTk.PhotoImage(file = 'bg.jpg')
 
@@ -12,7 +28,13 @@ bgLabel = Label(root, image= bgImage)
 bgLabel.place(x=0, y=0)
 #bgLabel.pack()
 
-heading = Label(root, text = 'USER LOGIN')
+heading = Label(root, text = 'USER LOGIN', font = ('Arial',13,'bold')
+                , bg = 'white', fg = 'firebrick')
 heading.place(x = 650, y = 120)
+
+usernameEntry = Entry(root, width = 23, font = ('Arial',11,'bold'),
+                       bd = 0, fg = 'firebrick')
+usernameEntry.place(x = 580, y = 200)
+usernameEntry.insert(0, 'Username')
 
 root.mainloop()
