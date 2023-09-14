02c2238af6b86d62ebd8d5f34af5fd5841638f76
Manoj Ghosh <manojghoshdk@gmail.com>
2023-09-14 12:00:43 +0530

    adding text on the page(USER LOGIN)

--- signin.py
+++ signin.py
@@ -2,11 +2,17 @@ from tkinter import *
 from PIL import ImageTk
 
 root = Tk()
+root.geometry('990x660+50+50')
 
 bgImage = ImageTk.PhotoImage(file = 'bg.jpg')
 
 bgLabel = Label(root, image= bgImage)
 
-bgLabel.grid(row = 0, column = 0)
+#bgLabel.grid(row = 0, column = 0)
+bgLabel.place(x=0, y=0)
+#bgLabel.pack()
+
+heading = Label(root, text = 'USER LOGIN')
+heading.place(x = 650, y = 120)
 
 root.mainloop()
