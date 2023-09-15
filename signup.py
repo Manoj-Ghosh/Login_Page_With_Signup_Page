86f995e663e631c31b9858f3d73c1ae180ffe354
Manoj Ghosh <manojghoshdk@gmail.com>
2023-09-15 10:55:27 +0530

    added username field

--- signup.py
+++ signup.py
@@ -18,8 +18,23 @@ frame.place(x = 453, y = 40)
 
 
 heading = Label(frame, text = 'Create An Account', font = ('Arial',23)
-                , bg = 'white', fg = 'firebrick')
+                , bg = 'white', fg = 'firebrick', background= 'white')
 heading.grid(row = 0, column=0, padx = 18, pady=10)
+
+emaillabel = Label(frame, text = 'Email', font = ('Arial',10), bg = 'white', fg = 'firebrick')
+emaillabel.grid(row=1, column=0, sticky='w', padx=10)
+
+
+emailEntry = Entry(frame, width= 30, font = ('Arial',13))
+emailEntry.grid(row=2, column=0)
+
+usernamelabel = Label(frame, text = 'Username', font = ('Arial',10), bg = 'white', fg = 'firebrick')
+usernamelabel.grid(row=3, column=0, sticky='w', padx=10)
+
+
+usernameEntry = Entry(frame, width= 30, font = ('Arial',13))
+usernameEntry.grid(row=4, column=0)
+
 
 signup_window.mainloop()
 
